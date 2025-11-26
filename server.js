a810bc822504ce3b69626bb3c5f8b27fd9d92d0b
Sergio <sergiosanchez.24@campuscamara.es>
2025-11-26 09:39:49 +0100

    cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 // Importar rutas
 app.use('/productos', require('./routes/productosRoutes'));
 app.use('/clientes', require('./routes/clientesRoutes'));
